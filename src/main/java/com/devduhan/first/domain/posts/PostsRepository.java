529dd85bc825d8933fd62b6b44b7d2fba8bcb6fc
DuhanMo <devduhanmo@gmail.com>
2021-01-08 15:18:57 +0900

    chap04 mustache 화면 구성 완료

--- src/main/java/com/devduhan/first/domain/posts/PostsRepository.java
+++ src/main/java/com/devduhan/first/domain/posts/PostsRepository.java
@@ -1,6 +1,13 @@
 package com.devduhan.first.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    //jpa에서 제공하지 않는 메소드는 이렇게 쿼리로 작성해도 됨
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
---
+++ src/main/java/com/devduhan/first/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.devduhan.first.web.dto;
+
+import com.devduhan.first.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
